9d63b71b72b817e146da306f0184c922dc3d3bc3
Harisk25 <kajtazovichkmail@gmail.com>
2024-02-13 12:27:48 -0700

    added comments to code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
     bool isFacingRight = true;
 
-    [Header ("Movement")]
+    [Header ("Movement")] //Header adds headers in unity to making sectiosn of  variables popout.
     public float moveSpeed = 5f;
     float horizontalMovement;
     bool keyDown = false;
@@ -69,10 +69,14 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+    /*
+     * ProcessGravity gives gravity to our player. The gravity directly impacts how fast our player falls
+     * so that our player doesnt seem like they are floaty. The gravity varibles can be edited in unity.
+     */
     private void ProcessGravity()
     {
         //falling gravity
-        if (rb.velocity.y < 0 && isGrounded == false)
+        if (rb.velocity.y < 0 && isGrounded == false) //Player needs to be on the ground and falling (player reached max jump height)
         {
             rb.gravityScale = baseGravity * fallSpeedMultiplier; //fall faster and faster
             rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed)); //max fall speed
@@ -82,44 +86,53 @@ public class PlayerMovement : MonoBehaviour
             rb.gravityScale = baseGravity;
         }
     }
-
+    /*
+     * ProcessWallSlide is the method that checks to see if the player is next to a wall while sliding.
+     * If the player is besided a wall and is moving towards that wall while falling it will cause wall sliding and reduce fall speed
+     */
     private void ProcessWallSlide()
     {
-        if (!isGrounded && wallCheck() & horizontalMovement != 0)
+        if (!isGrounded && WallCheck() & horizontalMovement != 0) //Player must be not grounded and next to a wall and not moving in the x - axis
         {
             isWallSliding = true;
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -wallSlideSpeed)); //Caps fall rate
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -wallSlideSpeed)); //Caps fall rate, this is a reduced speed
         }
         else
         {
             isWallSliding = false;
         }
     }
-
-    private void ProcessWallJump()
+    /*
+     * ProcessWallJump allows our player to jump after wall sliding. The player is unable to act (move in the x-axis) until a certian amount of time has passed.
+     */
+    private void ProcessWallJump() 
     {
-        if (isWallSliding)
+        if (isWallSliding) //Player needs to be wall sliding.
         {
-            isWallJumping = false;
-            wallJumpDirection = -transform.localScale.x;
-            wallJumpTimer = wallJumpTime;
+            isWallJumping = false; //Player Wall jumped, no longer sliding
+            wallJumpDirection = -transform.localScale.x; //Launch in the x direction
+            wallJumpTimer = wallJumpTime; //Start buffer timer, prevents contstant wall jumping
 
-            CancelInvoke(nameof(CancelWallJump));
+            CancelInvoke(nameof(CancelWallJump)); //End of our wall jump, player can input other actions
         }
         else if (wallJumpTimer > 0f)
         {
             wallJumpTimer -= Time.deltaTime;
         }
     }
-
+    /*
+     * CancelWallJump is an invoke method that is invoked when a player performs a wall jump
+     */
     private void CancelWallJump()
     {
         isWallJumping = false;
     }
-
-    public void Move(InputAction.CallbackContext context)
+    /*
+     * Move is used to make the player move in the x-axis.
+     */
+    public void Move(InputAction.CallbackContext context) 
     {
-        if (context.performed)
+        if (context.performed) //If statement needed to help fix animation issues when swithing from falling to idle animation.
         {
             horizontalMovement = context.ReadValue<Vector2>().x;
             keyDown = true;
@@ -131,10 +144,13 @@ public class PlayerMovement : MonoBehaviour
         }
 
     }
-
+    /*
+     * Jump is used to make our player jump under certain restrictions. Can jump multiple times with the increase in the max jump variable
+     * Jump also preforms the wall jump.
+     */
     public void Jump(InputAction.CallbackContext context)
     {
-        if (jumpsRemaining > 0)
+        if (jumpsRemaining > 0) //Check to see if we have jumps remaining
         {
             if (context.performed)
             {
@@ -171,26 +187,32 @@ public class PlayerMovement : MonoBehaviour
             Invoke(nameof(CancelWallJump), wallJumpTime + 0.1f); //Wall Jump will last 0.5 seconds and can jump again 0.6 seconds
         }
     }
-
+    /*
+     * GroundCheck makes sure that the player is on the ground.
+     * Used for checking if the player can jump.
+     */
     private void GroundCheck()
     {
         if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer)) //checks if set box overlaps with ground
         {
-            jumpsRemaining = maxJumps;
+            jumpsRemaining = maxJumps; //Resests jump remaining so that the player can jump again.
             isGrounded = true;
         }
         else
         {
             isGrounded = false;
         }
-        Debug.Log(isGrounded);
     }
-
-    private bool wallCheck()
+    /*
+     * WallCheck makes sure we are touching a wall to preform a wall slide.
+     */
+    private bool WallCheck()
     {
         return Physics2D.OverlapBox(wallCheckPos.position, wallCheckSize, 0, wallLayer);
     }
-
+    /*
+     * Flip just flips the player sprite.
+     */
     private void Flip()
     {
         if (isFacingRight && horizontalMovement < 0 || !isFacingRight && horizontalMovement > 0)
@@ -201,10 +223,15 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = ls;
         }
     }
+    /*
+     * OnDrawGizmosSelected draws our GroundCheck and WallCheck hit boxes so we can see how big they are.
+     */
     private void OnDrawGizmosSelected()
     {
+        //Groundcheck hit box
         Gizmos.color = Color.white;
         Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+        //Wallcheck hit box
         Gizmos.color = Color.blue;
         Gizmos.DrawWireCube(wallCheckPos.position, wallCheckSize);
     }
