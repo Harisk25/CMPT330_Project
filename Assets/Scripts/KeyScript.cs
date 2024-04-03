c09030a0327eaeec3b89e2c8cac2354d5adf9aaf
Harisk25 <kajtazovichkmail@gmail.com>
2024-04-03 15:32:02 -0600

    Finishing Touches

--- Assets/Scripts/KeyScript.cs
+++ Assets/Scripts/KeyScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class KeyScript : MonoBehaviour
 {
+	[SerializeField] private AudioClip collectSoundClip;
 	void OnTriggerEnter2D(Collider2D collision)
 	{
 
@@ -11,6 +12,7 @@ public class KeyScript : MonoBehaviour
 		{
 			var player = collision.gameObject.GetComponent<PlayerMovement>();
 			player.hasKey = true;
+			SoundFXManager.instance.PlaySoundFXClip(collectSoundClip, transform, 0.7f);
 			Destroy(gameObject);
 		}
 
